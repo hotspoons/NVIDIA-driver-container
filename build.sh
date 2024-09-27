a7a051f12ca61c902a133c7de4eee508e009c5fe
root <root@rocky-k8s-hosts-0.siomporas.com>
2024-09-27 01:22:35 +0000

    Updated build script to lock to minor version of OS. Really dumb that there isn't a symlink to the current Rocky version from their vault

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
 NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-535.104.12}"
 CONTAINER_REGISTRY="${CONTAINER_REGISTRY:-container-registry.siomporas.com}"
-RPM_BASE_URL="${RPM_BASE_URL:-https://dl.rockylinux.org/pub/rocky/8/BaseOS/x86_64/os/Packages}"
 OS_NAME="${OS_NAME:-$(cat /etc/os-release | grep ^ID= | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//')}"
 OS_RELEASE="${OS_RELEASE:-$(cat /etc/os-release | grep VERSION_ID | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//')}"
+RPM_BASE_URL="${RPM_BASE_URL:-https://dl.rockylinux.org/vault/rocky/${OS_RELEASE}/BaseOS/x86_64/os/Packages}"
 
 echo "Building NVIDIA Driver image version $NVIDIA_DRIVER_VERSION for ${OS_NAME}${OS_RELEASE} (kernel version $KERNEL_VERSION)\
  and deploying to registry $CONTAINER_REGISTRY, pulling kernel RPMs from base URL $RPM_BASE_URL"
